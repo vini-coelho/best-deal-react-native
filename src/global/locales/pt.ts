68541f063c4b7c64a48a9bee39fad1c7811ce4f3
Vinicius Coelho <vinicius_coelho@live.com>
2021-09-21 11:10:19 -0400

    BDL-18: moves locales to global dir

--- src/global/locales/pt.ts
+++ src/global/locales/pt.ts
@@ -2,6 +2,7 @@ export const pt = {
   WELCOME: 'Bem vindo!',
 
   GENERAL_ADD: 'Adicionar',
+  GENERAL_PRICE: 'Preço',
 
-  COMPARE_SCREEN_TITLE: 'Compare & Economize',
+  COMPARE_SCREEN_TITLE: 'Compare &\nEconomize',
 }
