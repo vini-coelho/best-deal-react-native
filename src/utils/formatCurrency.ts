6e9b9c6062831f5d2622f0333eb575ff6b1110a3
Vinicius Coelho <vinicius_coelho@live.com>
2021-09-26 11:36:31 -0400

    BDL-18: fix locales config

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,4 +1,7 @@
 import * as Localization from 'expo-localization';
+import 'intl';
+import 'intl/locale-data/jsonp/en-US'
+import 'intl/locale-data/jsonp/pt-BR'
 
 export function formatCurrency(value: number) {
   return new Intl.NumberFormat(Localization.locale, {
