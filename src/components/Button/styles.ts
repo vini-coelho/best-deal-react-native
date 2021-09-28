3333986caabf8b6cd51b0a872c9a627623b2004e
Vinicius Coelho <vinicius_coelho@live.com>
2021-09-28 19:11:15 -0400

    BDL-26: updates Button design

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,18 +2,23 @@ import styled from 'styled-components/native'
 import { RectButton } from 'react-native-gesture-handler';
 import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled(RectButton)`
-  width: 100%;
-  height: ${RFValue(56)}px;
-  align-items: center;
-  justify-content: center;
-`;
-
-interface TitleProps {
-  buttonStyle: 'red' | 'green' | 'white';
+interface Props {
+  buttonStyle: 'default' | 'white';
 }
 
-export const Title = styled.Text<TitleProps>`
+export const Container = styled(RectButton)<Props>`
+  width: 100%;
+  height: ${RFValue(56)}px;
+  border-radius: ${RFValue(15)}px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme, buttonStyle }) => buttonStyle === 'white'
+    ? theme.colors.white
+    : theme.colors.main
+  };
+`;
+
+export const Title = styled.Text<Props>`
   color: ${({ theme, buttonStyle }) => buttonStyle === 'white'
     ? theme.colors.title
     : theme.colors.white
